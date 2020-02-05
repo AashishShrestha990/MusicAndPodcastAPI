692e532702dac380d3dfae87837cc0b1df873d60
AashishShrestha990 <55848755+AashishShrestha990@users.noreply.github.com>
2020-02-05 18:33:04 +0545

    Find song based on user

--- routes/song.js
+++ routes/song.js
@@ -6,8 +6,8 @@ router.post('/add', (req, res, next) => {
     
         Posts.create({
             name: req.body.name,
-            uname:req.body.uname,
-            genre:req.body.genre,
+            uname: req.body.uname,
+            genre: req.body.genre,
             image: req.body.image,
         }).then((postsongs) => {
            // let token = jwt.sign({ _id: posty._id }, process.env.SECRET);
@@ -24,10 +24,33 @@ router.post('/add', (req, res, next) => {
 router.route('/')
     .get((req, res, next) => {
         Posts.find({})
-            .then((postweet) => {
+            .then((postsong) => {
                 res.statusCode = 200;
                 res.setHeader('Content-Type', 'application/json');
-                res.json(postweet);
+                res.json(postsong);
+            }).catch(next);
+    })
+    .put((req, res, next) => {
+        Posts.findOneAndUpdate({ author: req.user._id, _id: req.params.id }, { $set: req.body }, { new: true })
+            .then((reply) => {
+                if (reply == null) throw new Error("Song not found!");
+                res.json(reply);
+            }).catch(next);
+    })
+    .delete((req, res, next) => {
+        Posts.deleteMany({ author: req.user._id })
+            .then((status) => {
+                res.json(status);
+            }).catch(next);
+    });
+
+router.route('/:uname')
+    .get((req, res, next) => {
+        Posts.find({uname: req.params.uname})
+            .then((postsong) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(postsong);
             }).catch(next);
     })
     .put((req, res, next) => {
@@ -43,7 +66,6 @@ router.route('/')
                 res.json(status);
             }).catch(next);
     });
-
 
 
 module.exports = router;
